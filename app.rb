718d956c133bfabc058462762ea4a848561c424e
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-08-22 16:37:28 +0100

    add session and PRG pattern

--- app.rb
+++ app.rb
@@ -2,13 +2,21 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post "/names" do
-    @player01 = params[:player01]
-    @player02 = params[:player02]
+    session[:player01] = params[:player01]
+    session[:player02] = params[:player02]
+    redirect "/play"
+  end
+
+  get "/play" do
+    @player01 = session[:player01]
+    @player02 = session[:player02]
     erb :play
   end
 
