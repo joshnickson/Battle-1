06a10f50325342f43d16fb23e38c22ddd5dd429b
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-08-22 15:38:52 +0100

    add player name form

--- app.rb
+++ app.rb
@@ -3,8 +3,13 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
   get '/' do
-    'Hello Battle'
-    'Testing infrastructure working!'
+    erb :index
+  end
+
+  post "/names" do
+    @player01 = params[:player01]
+    @player02 = params[:player02]
+    erb :play
   end
 
   run! if app_file == $0
--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -1,6 +1,15 @@
-feature "Home Page" do
-  scenario "test infrastructure" do
+feature "Submit names" do
+  
+  # As two Players,
+  # So we can play a personalised game of Battle,
+  # We want to Start a fight by entering our names and seeing them
+  scenario "players enter their names" do
     visit("/")
-    expect(page).to have_content "Testing infrastructure working!"
+    fill_in :player01, with: "Hagrid"
+    fill_in :player02, with: "Dumbledore"
+
+    click_button "Submit"
+    expect(page).to have_content("Hagrid", "Dumbledore")
   end
+
 end
