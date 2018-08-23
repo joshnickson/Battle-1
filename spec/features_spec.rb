bdc7bb92fbf65713b799b630a848540ef2c15f13
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-08-23 13:21:48 +0100

    add HP for player 2

--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -12,4 +12,16 @@ feature "Submit names" do
     expect(page).to have_content("Hagrid", "Dumbledore")
   end
 
+  # As Player 1,
+  # So I can see how close I am to winning
+  # I want to see Player 2's Hit Points
+  scenario "can see Player 2's Hit Points" do 
+    visit("/")
+    fill_in :player01, with: "Hagrid"
+    fill_in :player02, with: "Dumbledore"
+    click_button "Submit"
+
+    expect(page).to have_content("HP = 100")
+  end
+
 end
